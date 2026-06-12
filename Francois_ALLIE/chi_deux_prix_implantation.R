33a5fa99f13342eeba481cbbfb284f7796cbb348
FrancoisAllie <152420852+FrancoisAllie@users.noreply.github.com>
2026-06-12 12:02:47 +0200

    Correction du code par rapport à la colonne tarification qui est devenue tarification_eur_kWh

--- Francois_ALLIE/chi_deux_prix_implantation.R
+++ Francois_ALLIE/chi_deux_prix_implantation.R
@@ -3,38 +3,29 @@ library(readr)
 
 data <- read_csv("./IRVE_clean.csv")
 
-
 donnees_visu <- data %>%
   mutate(implantation_station = case_when(
+    # On modifie le nom des colonnes pour qu'ils soient plus court et plus lisible pour le mosaicplot 
     implantation_station == "Parking privé à usage public" ~ "P.Privé(Public)",
     implantation_station == "Parking privé réservé à la clientèle" ~ "P.Privé(Client)",
     implantation_station == "Parking public" ~ "P.Public",
     
-    # On garde les noms courts existants tels quels (ex: "Voirie")
+    # On garde le nom "Voirie" car il est assez court
     TRUE ~ implantation_station 
     ),
 
-    # On enlève exactement le texte "€/kWh" (remplacé par du vide "")
-    prix_num = gsub("€/kwh", "", tarification, ignore.case = TRUE),
-    
-    # L'argument ignore.case = TRUE permet d'effacer le texte qu'il soit en majuscule ou en minuscules
-    # Car dans le CSV on a parfois "kwh" et parfois "kWh" 
-    
-    # On enlève les espaces vides restants et on convertit en numérique
-    prix_num = as.numeric(trimws(prix_num))
+    # Création de la variable qualitative de tarification 
+    # Car le test du chi-deux s'effectue avec deux variables qualitative, or le prix est au départ une variable quantitative
+    tarification_qualitative = case_when(
+      tarification_eur_kWh == 0 ~ "Gratuit",
+      tarification_eur_kWh > 0 ~ "Payant"
+    )
+
   )
 
-# On crée une nouvelle variable qualitative à partir de la variable quantitative du prix
-# Car le test du chi-deux s'effectue avec deux variables qualitative, or le prix est au départ une variable quantitative
-
-data_prix_qualitatif <- donnees_visu %>%
-  mutate(tarification = case_when(
-    prix_num == 0 ~ "Gratuit",
-    prix_num > 0 ~ "Payant"
-  ))
 
 # Le test du Chi-2 devient possible
-tableau_croise <- table(data_prix_qualitatif$implantation_station, data_prix_qualitatif$tarification)
+tableau_croise <- table(donnees_visu$implantation_station, donnees_visu$tarification_qualitative)
 chisq.test(tableau_croise)
 
 # Le mosaicplot 
