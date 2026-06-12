6792860c94f36cc5535581bb46b28187eae8cdac
FrancoisAllie <152420852+FrancoisAllie@users.noreply.github.com>
2026-06-12 12:08:33 +0200

    ajout de quelques commentaires

--- Francois_ALLIE/correlation_pdc_implantation.R
+++ Francois_ALLIE/correlation_pdc_implantation.R
@@ -5,6 +5,7 @@ data <- read_csv("./IRVE_clean.csv")
 
 # On regarde si le nombre de points de charge est corrélé au type d'implantation
 
+# ggplot de R fonctionne par "couches", on ajoute d'abord les axes (1re couche), puis le boxplot (2e couche) avec "+" etc...
 ggplot(data, aes(x = implantation_station, y = nbre_pdc)) +
   # Création du boxplot
   geom_boxplot(fill = "lightblue", color = "darkblue", outlier.color = "red", outlier.alpha = 0.5) +
@@ -16,6 +17,7 @@ ggplot(data, aes(x = implantation_station, y = nbre_pdc)) +
   # Inclinaison des étiquettes de l'axe X à 45 degrés pour qu'elles ne se chevauchent pas
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
 
+  
   labs(title = "Nombre de points de charge selon le type d'implantation",
        x = "Type d'implantation",
        y = "Nombre de points de charge (PDC)")
